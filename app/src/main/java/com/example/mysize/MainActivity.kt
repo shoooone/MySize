0155e6823aa846027466059f243019e940084e63
shoooone <pride.a.s.w.12.01@gmail.com>
2019-02-26 14:36:16 +0900

    保存処理まで

--- app/src/main/java/com/example/mysize/MainActivity.kt
+++ app/src/main/java/com/example/mysize/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.mysize
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editNeck: EditText
+    private lateinit var editSleeve: EditText
+    private lateinit var editWaist: EditText
+    private lateinit var editInseam: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editNeck = findViewById(R.id.neck)
+        editSleeve = findViewById(R.id.sleeve)
+        editWaist = findViewById(R.id.waist)
+        editInseam = findViewById(R.id.inseam)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        editNeck.setText(pref.getString(NECK, ""))
+        editSleeve.setText(pref.getString(SLEEVE, ""))
+        editWaist.setText(pref.getString(WAIST, ""))
+        editInseam.setText(pref.getString(INSEAM, ""))
+
+    }
+
+    fun onTappedSaveButton(view: View) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.putString(NECK, editNeck.text.toString())
+        editor.putString(SLEEVE, editSleeve.text.toString())
+        editor.putString(WAIST, editWaist.text.toString())
+        editor.putString(INSEAM, editInseam.text.toString())
+        editor.apply()
+    }
+
+
+    companion object {
+        const val NECK = "NECK"
+        const val SLEEVE = "SLEEVE"
+        const val WAIST = "WAIST"
+        const val INSEAM = "INSEAM"
     }
 }
